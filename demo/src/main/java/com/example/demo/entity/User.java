42a9db008a7874d1b44f69b9d8fe770f4518184d
Devon08-lab <devon08yad@gmail.com>
2025-01-31 12:09:53 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,44 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String userid;
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/*public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}*/
+	private String email;
+	
+
+}
+ 
