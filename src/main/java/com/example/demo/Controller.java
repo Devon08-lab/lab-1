e22b220ea8ad02ee0ccd76f42ccb89cf3fa80972
Devon08-lab <devon08yad@gmail.com>
2025-02-13 01:17:55 +0530

    Update Controller.java

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -8,6 +8,7 @@ public class Controller {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("hello world from controller");
 	}
 
 }
